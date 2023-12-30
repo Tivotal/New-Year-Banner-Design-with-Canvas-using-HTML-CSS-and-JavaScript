afc020caf0904781ad21ea07898027aeaad69c73
Tivotal <121335284+Tivotal@users.noreply.github.com>
2023-12-30 14:32:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+/* Created by Tivotal */
+
+let canvas = document.querySelector("#canvas");
+
+let ctx = canvas.getContext("2d");
+let winWidth = window.innerWidth;
+let winHeight = window.innerHeight;
+
+//Set Canvas and Background Color;
+canvas.width = winWidth;
+canvas.height = winHeight;
+ctx.fillStyle = "#000";
+ctx.fillRect(0, 0, winWidth, winHeight);
+
+//Glow effect;
+ctx.shadowBlur = 10;
+ctx.shadowColor = "white";
+
+function animate() {
+  //Random position and size of stars;
+  let x = winWidth * Math.random();
+  let y = winHeight * Math.random();
+  let r = 2.5 * Math.random();
+
+  //Draw the stars;
+  ctx.beginPath();
+  ctx.fillStyle = "#fff";
+  ctx.arc(x, y, r, 0, Math.PI * 2);
+  ctx.fill();
+
+  //Using setTimeout instead of window.requestAnimationFrame for slower speed... window.requestAnimationFrame is approximately equal to setTimeout(func, 17);
+  setTimeout(animate, 100);
+}
+
+animate();
